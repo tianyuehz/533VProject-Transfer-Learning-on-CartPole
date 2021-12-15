2f9e0c6a50ce4feffc08149e0b7a99c706627936
Daniel Ajisafe <ajisafedaniel@gmail.com>
2021-12-14 16:44:11 -0800

    Add files via upload

--- OSI.py
+++ OSI.py
@@ -25,15 +25,15 @@ from models import OSI
 
 def main(args):
 
-    n_mus = 2 #args.n_mus
+    n_mus = 4 #args.n_mus
     act_input = 2
     state_dim = 4
     obs_dim_OSI = state_dim + act_input
     obs_dim_input = state_dim + n_mus
 
     # Load trained OSI
-    model_path = 'save_OSI_models/OSI_true_weight_dec9.pth'
-    OSI_estimator = torch.load(model_path)
+    # model_path = 'save_OSI_models/OSI_true_weight_dec9.pth'
+    # OSI_estimator = torch.load(model_path)
 
     out_dim = n_mus
 
@@ -44,13 +44,10 @@ def main(args):
     # Initialize the B buffer
     bufB = B_Buffer(H_dim, mu_dim, stretch_size, args)
 
-    # actor critic 
-    discrete = True
-    ac = ActorCritic(obs_dim_input, act_input, discrete).to(args.device)
-
+    '''Collecting Data with trained UP Policy'''
     # Load trained Policy
-    #model_path = 'saved_policy_models/policy_weight_dec2.pth' # Dan model
-    #ac = torch.load(model_path)
+    model_path = 'saved_policy_models/UP_k30_ep250_nmu4_full.pth' # Dan model
+    ac = torch.load(model_path)
 
     for k_index in range(args.K):
         # create environment 
@@ -94,19 +91,19 @@ def main(args):
                 o = next_o
 
             '''curious, why do we need this line?'''
-            o, ep_ret, ep_len = env.reset(), 0, 0 ; #print("o", o)
+            #o, ep_ret, ep_len = env.reset(), 0, 0 ; #print("o", o)
 
             #print("env_params before", env_params)
             for t_index in range(args.T):
 
-                if args.mismatch_OSI:
-                    H_input = torch.as_tensor(H, dtype=torch.float32).to(args.device).reshape(1,-1)
-                    #print("H_input", H_input.reshape(1,-1).shape)
+                # if args.mismatch_OSI:
+                #     H_input = torch.as_tensor(H, dtype=torch.float32).to(args.device).reshape(1,-1)
+                #     #print("H_input", H_input.reshape(1,-1).shape)
 
-                    '''using OSI as estimator'''
-                    keep_true = env_params
-                    out = OSI_estimator(H_input)
-                    env_params = out.view(-1).detach().numpy(); #print("env_params", env_params)
+                #     '''using OSI as estimator'''
+                #     keep_true = env_params
+                #     out = OSI_estimator(H_input)
+                #     env_params = out.view(-1).detach().numpy(); #print("env_params", env_params)
 
                 # pops out the oldest
                 H.pop(0)
@@ -120,27 +117,27 @@ def main(args):
                 # append the newest
                 H.append(np.append(next_o.reshape(-1), a))
 
-                if args.mismatch_OSI:
-                    env_params = keep_true
+                # if args.mismatch_OSI:
+                #     env_params = keep_true
 
                 #print("env_params before", env_params);stop
                 # store into H and mu into Buffer B
                 bufB.store(H, env_params, args)
 
 
-    # batch = bufB.get()
-    # #save information
-    # np.save('save_data/B_buffer_H_high_var_retrain_mismatch.npy', batch['H']) # save
-    # np.save('save_data/B_buffer_mu_high_var_retrain_mismatch.npy', batch['mu']) # save
+    batch = bufB.get()
+    #save information
+    np.save('save_data/B_buffer_H_full.npy', batch['H']) # save
+    np.save('save_data/B_buffer_mu_full.npy', batch['mu']) # save
     
-    '''load information'''
+    ''' Training OSI with Data Rollout from UP'''
     # Part 1 data
     # B_buffer_H = np.load('save_data/B_buffer_H_high_variance.npy'); print("B_buffer_H", B_buffer_H[0,0])
     # B_buffer_mu = np.load('save_data/B_buffer_mu_high_variance.npy'); print("B_buffer_mu", B_buffer_mu[0,0])
     
     # Part 2 Data
-    B_buffer_H = np.load('save_data/B_buffer_H_high_var_retrain_mismatch.npy'); #print("B_buffer_H", B_buffer_H[0,0])
-    B_buffer_mu = np.load('save_data/B_buffer_mu_high_var_retrain_mismatch.npy'); #print("B_buffer_mu", B_buffer_mu[0,0]);#stop
+    B_buffer_H = np.load('save_data/B_buffer_H_full.npy'); #print("B_buffer_H", B_buffer_H[0,0])
+    B_buffer_mu = np.load('save_data/B_buffer_mu_full.npy'); #print("B_buffer_mu", B_buffer_mu[0,0]);#stop
 
     # extract the dimensions automatically
     T = B_buffer_H.shape[-2]
@@ -188,12 +185,12 @@ def main(args):
     model = OSI(H_dim*T, out_dim).to(device)
 
     # for re-training
-    if args.mismatch_OSI:
-        import copy
-        model_path = 'save_OSI_models/OSI_true_weight_dec9.pth'
-        OSI_estimator = torch.load(model_path)
-        #load weights from loaded model
-        model.load_state_dict(copy.deepcopy(OSI_estimator.state_dict()))
+    # if args.mismatch_OSI:
+    #     import copy
+    #     model_path = 'save_OSI_models/OSI_true_weight_dec9.pth'
+    #     OSI_estimator = torch.load(model_path)
+    #     #load weights from loaded model
+    #     model.load_state_dict(copy.deepcopy(OSI_estimator.state_dict()))
 
     optimizer = Adam(model.parameters(), lr=lr)
     criterion = nn.L1Loss() 
@@ -210,9 +207,9 @@ def main(args):
         print(f"Val Loss: {val_epoch_loss:.4f}")
 
 
-    # project_dir = "/home/daniel/Documents/Msc/ubc/UBC Lectures/CPSC 533V/Project/533VProject_modified/"
-    # file = project_dir + 'save_OSI_models/OSI_retrain_mismatch_weight_dec9.pth'
-    # torch.save(model, file)
+    project_dir = "/home/daniel/Documents/Msc/ubc/UBC_Lectures/CPSC_533V/Project/533VProject_modified/"
+    file = project_dir + 'save_OSI_models/OSI_dec14.pth'
+    torch.save(model, file)
     
 
 
